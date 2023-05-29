17bb7364808e71d21228e02eab862394c424b4a1
Chirag <chiragmunjal550@gmail.com>
2023-05-29 09:06:42 +0530

    postverse application using React js and Sanity Io - Files has been setup sucessfully , schemas defined ,installed all dependencies

---
+++ backend/schemas/comment.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    {name: 'postedby', title: 'PostedBy', type: 'postedBy'},
+    {
+      name: 'comment',
+      title: 'Comment',
+      type: 'string',
+    },
+  ],
+}
